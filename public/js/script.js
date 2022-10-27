3d1f5c4f7478c83f947e059618019e0f1b8ffe04
mary <macbookpro@macbooks-MacBook-Pro.local>
2022-10-27 15:29:07 +0700

    dr.asa 2

--- public/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('.hamburger').on('click', function() {
-        $('.nav-grid').toggleClass("open");
-    });
-});
-
-function myFunction(x) {
-    x.classList.toggle("change");
-}
